78d49e8665afe92e9fa0ee720319c8514cd4dcec
Kevin Leung <kevinresol@gmail.com>
2019-04-03 12:59:04 +0800

    Update props

--- src/react/google_maps/DrawingManager.hx
+++ src/react/google_maps/DrawingManager.hx
@@ -1,11 +1,18 @@
 package react.google_maps;
 
 import google.maps.*;
+import google.maps.drawing.*;
 import react.ReactComponent;
 
 #if react_global @:native('window["react-google-maps-api"].DrawingManager') #else @:jsRequire('@react-google-maps/api', 'DrawingManager') #end
 extern class DrawingManager extends ReactComponentOfProps<{
-	?drawingMode:Dynamic,
-	?options:Dynamic,
+	?options:DrawingManagerOptions,
+	?drawingMode:OverlayType,
+	?onCircleComplete:Circle->Void,
+	?onMarkerComplete:Marker->Void,
+	?onOverlayComplete:OverlayCompleteEvent->Void,
 	?onPolygonComplete:Polygon->Void,
+	?onPolylineComplete:Polyline->Void,
+	?onRectangleComplete:Rectangle->Void,
+	?onLoad:DrawingManager->Void,
 }> {}
